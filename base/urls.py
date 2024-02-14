06552eccc2fa879d862447efb368add8d2cf2c41
joshwaoti <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>
2024-02-14 13:00:23 +0300

    🧑‍💻 configured the drf_yasg

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework import permissions
 from .views import UserViewSet, ProjectViewSet
+# from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Innovation API",
+        default_version='v1',
+        description="This API contains information for various users with their projects",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourdomain.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -12,6 +29,8 @@ router.register(r'projects', ProjectViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
 
 
